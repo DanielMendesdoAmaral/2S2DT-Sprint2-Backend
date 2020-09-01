3ece68b2f52101705e06462b6cf5de632f22421d
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2020-09-01 17:30:53 -0300

    Aula 2

---
+++ Aulas/2- Domain Driven Design (DDD)/API_Boletim/Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Boletim.Domains;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno Cadastrar(Aluno aluno);
+        List<Aluno> Ler();
+        Aluno BuscarPorId(int id);
+        Aluno Alterar(Aluno aluno);
+        Aluno Excluir(Aluno aluno);
+    }
+}
---
+++ Aulas/2- Domain Driven Design (DDD)/API_Boletim/Repositories/AlunoRepositorie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Boletim.Domains;
+using API_Boletim.Interfaces;
+
+namespace API_Boletim.Repositories
+{
+    public class AlunoRepositorie : IAluno
+    {
+        public Aluno Alterar(Aluno aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> Ler()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
